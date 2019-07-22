18a66fd82375a332d81d1e0182274bc8edd6ba05
togoboy8 <cedrichillah@gmail.com>
2019-07-22 16:56:45 -0400

    add first draft of the employee salary script

---
+++ weeklysalary.py
@@ -0,0 +1,33 @@
+#!/usr/env/python
+
+name = input('What is your name?:  ')
+
+hours = input('How many hours did you work this week?:  ')
+
+hrate = input('Enter your hourly rate?:  ')
+
+ot = int(hours) - 40
+
+otsalary = 1.5 * int(ot)
+
+weeklysalary = int(hours) * int(hrate)
+
+if int(hours) <= 40:
+    print(name + " has worked " +  str(hours) + " hours this week.\n")
+    
+    print('Great Job for not going overtime!!\n')
+    
+elif int(hours) > 40:
+    
+    print(name + " has worked " +  str(hours) + " hours this week.\n")
+    
+    print("Try not to go overtime!! YOUR ARE MESSING UP WITH THE COMPANY'S MONEY\n")
+     
+    
+    print('Your salary for this week is ' + str(weeklysalary) + ' +  ' + str(ot) + ' extra hours of overtime\n')
+    
+    weeklysalary += otsalary
+    
+    print('Anyways, you get to fill up your pocket with an extra $' + str(otsalary) + ' and a total salary of: $' + str(weeklysalary))
+    
+    
